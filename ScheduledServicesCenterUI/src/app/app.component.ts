78d309e51e52f932b6d22da32435ed60f27bb55b
Nouri <nouryman999@gmail.com>
2024-09-28 20:26:04 +0300

    create form sector

--- ScheduledServicesCenterUI/src/app/app.component.ts
+++ ScheduledServicesCenterUI/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WebsiteInterfaceComponent } from './website-interface/website-interface.component';
+import { SectorComponent } from './sector/sector.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,WebsiteInterfaceComponent],
+  imports: [RouterOutlet,WebsiteInterfaceComponent,SectorComponent,LoginComponent],
   template: `<app-website-interface></app-website-interface>`,
   styleUrl: './app.component.css'
 })
